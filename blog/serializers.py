bc56c86f26b9d8749269844f0198794c5308bcda
EAGLEEARTH <ufukcicek199@gmail.com>
2026-08-03 11:41:55 +0300

    push

--- blog/serializers.py
+++ blog/serializers.py
@@ -39,9 +39,23 @@ class BlogPostListSerializer(serializers.ModelSerializer):
 
 
 class BlogPostDetailSerializer(serializers.ModelSerializer):
+    cover_image = serializers.SerializerMethodField()
+
     class Meta:
         model = BlogPost
         fields = "__all__"
+
+    def get_cover_image(self, obj):
+        if obj.cover_image:
+            request = self.context.get("request")
+            if request:
+                return request.build_absolute_uri(obj.cover_image.url)
+            return obj.cover_image.url
+        import re
+        match = re.search(r'<img[^>]+src=["\']([^"\']+)["\']', obj.content or "")
+        if match:
+            return match.group(1)
+        return None
 
 
 class BlogPostCreateSerializer(serializers.ModelSerializer):
